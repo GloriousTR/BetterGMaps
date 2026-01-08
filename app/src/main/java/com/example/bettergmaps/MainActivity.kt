19901416ef9509369d5f2dcb8d2c73cfc9b3af67
GloriousTR <87311965+GloriousTR@users.noreply.github.com>
2026-01-08 08:08:26 +0300

    Mayor update

--- app/src/main/java/com/example/bettergmaps/MainActivity.kt
+++ app/src/main/java/com/example/bettergmaps/MainActivity.kt
@@ -8,6 +8,9 @@ import android.media.AudioManager
 import android.media.ToneGenerator
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
