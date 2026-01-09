4fe092a3defe0ddfa4a3d2d2adf3de9f7cc3ac46
GloriousTR <87311965+GloriousTR@users.noreply.github.com>
2026-01-09 16:42:22 +0300

    Android Auto Fix

--- app/src/main/java/com/example/bettergmaps/car/MyCarAppService.kt
+++ app/src/main/java/com/example/bettergmaps/car/MyCarAppService.kt
@@ -3,6 +3,7 @@ package com.example.bettergmaps.car
 import androidx.car.app.CarAppService
 import androidx.car.app.Session
 import androidx.car.app.SessionInfo
+import androidx.car.app.validation.HostValidator
 
 class MyCarAppService : CarAppService() {
     override fun createHostValidator(): HostValidator {
