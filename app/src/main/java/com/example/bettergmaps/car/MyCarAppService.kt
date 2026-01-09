053545f93851d71617030c86be11bb1f85f33041
GloriousTR <87311965+GloriousTR@users.noreply.github.com>
2026-01-09 16:38:44 +0300

    Android Auto Fix

--- app/src/main/java/com/example/bettergmaps/car/MyCarAppService.kt
+++ app/src/main/java/com/example/bettergmaps/car/MyCarAppService.kt
@@ -2,18 +2,14 @@ package com.example.bettergmaps.car
 
 import androidx.car.app.CarAppService
 import androidx.car.app.Session
-import androidx.car.app.validation.HostValidator
+import androidx.car.app.SessionInfo
 
 class MyCarAppService : CarAppService() {
     override fun createHostValidator(): HostValidator {
-        return HostValidator.Builder(applicationContext)
-            .addAllowedHost("androidx.car.app.sample")
-            .addAllowedHost("com.google.android.projection.gearhead")
-            .addAllowedHost("com.android.car")
-            .build()
+        return androidx.car.app.validation.HostValidator.ALLOW_ALL_HOSTS_IN_DEBUG
     }
 
-    override fun onCreateSession(): Session {
+    override fun onCreateSession(sessionInfo: SessionInfo): Session {
         return MyCarSession()
     }
 }
